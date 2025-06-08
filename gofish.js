164951e7721e7996bf3461b80bc2522ddcd94796
TdL752 <tylerdamon1@aol.com>
2025-06-08 01:08:08 +0000

    Major changes game updates

--- gofish.js
+++ gofish.js
@@ -6,6 +6,9 @@ const dealerCardsDiv = document.getElementById("dealer-cards");
 const dealerCollectedCards = document.getElementById("collected-dealer-cards");
 const playerCollectedCards = document.getElementById("collected-player-cards");
 const dealerDialog = document.getElementById("dealer-dialog");
+const backgroundOptionMenu = document.getElementById("select-menu");
+const backgroundOption = document.querySelectorAll(".option");
+const deck = document.getElementById("deck");
 
 let dealerCardValues = [];
 let playerCardValues = [];
@@ -25,8 +28,7 @@ const updateCardId = () => {
         dealerCards[i].id = `dealer-${i}`;
     };
 
-    console.log("New player array: ", playerCardValues);
-    console.log("New dealer array: ", dealerCardValues);
+    backgroundChange();
     return;
 };
 
@@ -55,12 +57,13 @@ const dealPlayerCards = () => {
         dealerCardsDiv.appendChild(newEl);
         dealerCardValues.push(randomNum);
     };
+
+    backgroundChange();
     return;
 };
 
 
-const cardClick = (target) => {
-    updateCardId();
+const cardClick = (target, id) => {
     let firstMatch = true;
 
     if(fishFirst) {
@@ -73,20 +76,34 @@ const cardClick = (target) => {
 
     let goFish = true;
     console.log("Card clicked: ", target);
-    
 
-    for(let i = dealerCardValues.length; i > 0; i--) {
-        if(dealerCardValues[i] === playerCardValues[target.id] && firstMatch) {
-            console.log("Card clicked match: ", dealerCards[i]);
+    /*
+        if dealer array number = id of card clicked
+
+        remove "playerCard" class - this will effect style
+        add clicked card to collected cards div
+
+        remove value from player array using target id
+        remove value from dealer array using i
+        remove dealer card i from the dealer card div
+    */
+
+    for(let i = dealerCardValues.length - 1; i >= 0; i--) {
+        if(dealerCardValues[i] === playerCardValues[id] && firstMatch) {
+            const dealerCards = document.querySelectorAll(".dealer-card");
+            const playerCards = document.querySelectorAll(".player-card");
+            console.log("--problem point passed--");
+            console.log("Card clicked match: ", dealerCardValues[i]);
+            target.classList.remove("player-card");
             playerCollectedCards.appendChild(target);
-            playerCardValues.splice(target.id, 1);
+            playerCardValues.splice(id, 1);
             dealerCardValues.splice(i, 1);
             dealerCards[i].remove();
             goFish = false;
-            target.classList.remove("player-card");
             updateCardId();
             firstMatch = false
-        } else if(dealerCardValues[i] === playerCardValues[target.id] && !firstMatch) {
+        } else if(dealerCardValues[i] === playerCardValues[id] && !firstMatch) {
+            const dealerCards = document.querySelectorAll(".dealer-card");
             console.log("Card clicked second match: ", dealerCards[i]);
             dealerCardValues.splice(i, 1);
             dealerCards[i].remove();
@@ -128,6 +145,7 @@ const goFishBtn = () => {
     goFish = false;
     fishFirst = false;
     dealerTurn = true;
+    backgroundChange();
     updateCardId();
     opAi();
 };
@@ -146,10 +164,10 @@ const opAi = () => {
     let dealerFish = true;
     let randomNum = Math.floor(Math.random() * dealerCardValues.length);
 
-    if(lastNumCall === randomNum && randomNum < dealerCardValues.length) {
+    if(lastNumCall === randomNum && randomNum < dealerCardValues.length - 1) {
         console.log("last number < length");
         randomNum += 1;
-    } else if(lastNumCall === randomNum && randomNum === dealerCardValues.length) {
+    } else if(lastNumCall === randomNum && randomNum === dealerCardValues.length - 1) {
         console.log("last number = length");
         randomNum -= 1;
     };
@@ -158,8 +176,6 @@ const opAi = () => {
     let pickCardValue = dealerCardValues[randomNum];
     console.log("dealer card pick", pickCardValue);
 
-    
-
     dealerDialog.innerHTML = `
         <p>
             Do you have any ${pickCardValue}s?
@@ -167,9 +183,9 @@ const opAi = () => {
     `;
 
     setTimeout(() => {
-        for(let i = 0; i < playerCardValues.length; i++) {
+        for(let i = playerCardValues.length - 1; i >= 0; i--) {
             if(playerCardValues[i] === pickCardValue) {
-                console.log("card match", playerCards[i]);
+                console.log("card match", playerCardValues[i]);
                 playerCardValues.splice(i, 1);
                 dealerCardValues.splice(randomNum, 1);
                 dealerCollectedCards.appendChild(document.getElementById(`dealer-${randomNum}`));
@@ -183,9 +199,10 @@ const opAi = () => {
                 `;
                 dealerFish = false;
                 dealerTurn = true;
+                console.log("player array after match: ", playerCardValues);
+                console.log("dealer array after match: ", dealerCardValues);
                 updateCardId();
             };
-
         };
 
         dealerDialog.innerHTML = ``;
@@ -198,7 +215,6 @@ const opAi = () => {
             opAi();
             return;
         };
-
         console.log("AI TURN OVER");
 
     }, 2000);
@@ -214,6 +230,7 @@ const dealerGoFish = () => {
     addCard.id = `${dealerCardValues.length}`;
     addCard.classList.add("dealer-card");
     dealerCardsDiv.appendChild(addCard);
+    backgroundChange();
     updateCardId();
     return;
 };
@@ -233,12 +250,32 @@ restartBtn.addEventListener("click", () => {
 
 playerCardsDiv.addEventListener("click", (event) => {
     updateCardId();
-
-    if(event.target.classList.contains("face-card")) {
-        cardClick(event.target);
-        return;
-    } else {
-        console.log("event listener issue 189");
-        return;
-    };
+    console.log(event.target.id, event.target);
+    cardClick(event.target, event.target.id);
+    return;
 });
+
+
+const backgroundChange = () => {
+    const dealerCards = document.querySelectorAll(".dealer-card");
+
+    if(backgroundOptionMenu.value === "fish-card") {
+        dealerCards.forEach(card => {
+            card.style.backgroundImage = `url("https://res.cloudinary.com/dbuwekmex/image/upload/c_fill,w_65,h_100/v1749327259/fish-pattern-wallpaper-background_eoa0wo.jpg")`;
+
+        });
+        
+        deck.style.backgroundImage = `url("https://res.cloudinary.com/dbuwekmex/image/upload/c_fill,w_65,h_100/v1749327259/fish-pattern-wallpaper-background_eoa0wo.jpg")`;
+        
+    } else if(backgroundOptionMenu.value === "red-card") {
+        dealerCards.forEach(card => {
+            card.style.backgroundImage = `url("https://res.cloudinary.com/dbuwekmex/image/upload/v1748872353/Screenshot_2025-06-02_9.51.19_AM_insiek.png")`;
+        });
+
+        deck.style.backgroundImage = `url("https://res.cloudinary.com/dbuwekmex/image/upload/v1748872353/Screenshot_2025-06-02_9.51.19_AM_insiek.png")`;
+    };
+};
+
+document.addEventListener('DOMContentLoaded', backgroundChange)
+
+backgroundOptionMenu.addEventListener('change', backgroundChange);
